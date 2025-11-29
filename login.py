34626fd0aa33ef544267e99adb68ef2ee820e668
yuhongkuo <victor167036@gmail.com>
2025-11-29 20:35:03 +0800

    Update login.py

--- login.py
+++ login.py
@@ -5,15 +5,17 @@ with sync_playwright() as p:
     page = browser.new_page(user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
 
     # Step 1: 開啟頁面
-    page.goto("https://www.facebook.com/")
+    page.goto("https://www.saucedemo.com/")
 
     # Step 2: 輸入帳號密碼
-    page.fill("#email", "victor167036@gmail.com")
-    page.fill("#pass", "win167036")
+    page.fill("#user-name", "standard_user")
+    page.fill("#password", "secret_sauce")
 
     # Step 3: 點擊登入
-    page.click("button[name='login']")
+    page.click("#login-button")
 
     page.wait_for_load_state("networkidle")
 
     browser.close()
+
+
